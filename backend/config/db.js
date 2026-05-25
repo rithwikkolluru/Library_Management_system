baa9d51d180c39ade7557a6c568368fd7b10ebe5
RithvikKolluru <rithwikkolluru7@gmail.com>
2026-05-25 13:47:10 +0530

    Remove hardcoded database credentials
    Removed sensitive database credentials from db.js.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1234567",
-    database: "library_db"
+    password: "",
+    database: ""
 });
 
 db.connect((err) => {
